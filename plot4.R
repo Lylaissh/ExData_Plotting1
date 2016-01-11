3069c78137708ccbf80368d7eb74549c685c3447
lylaissh <sneh.km@gmail.com>
2016-01-10 18:07:51 -0600

    ExData_Plotting1 Assignment

--- plot4.R
+++ plot4.R
@@ -1,35 +1,54 @@
+## This program reads household_power_consumption.txt file to a data frame and 
+## plots the Global Active Power Histogram
+
+# Set working directory to folder where household_power_consumption.txt resides.
+setwd("D:/Coursera/ExploratoryData")
+
+# Read few rows of data in to variable pow_consumption to read column names
+pow_consumption <- read.csv("household_power_consumption.txt",nrows=10, sep=";")
 
 
-   my_data <- read.csv("household_power_consumption.txt", sep=";",colClasses="character")
-   my_data <- my_data[1:100000,]
-   good_data <- my_data
-   good_data[,1] <- as.Date(good_data[,1], "%d/%m/%Y")
-  
-   subset_data1 <- subset(good_data,good_data$Date=="2007-02-01")
-   subset_data2 <- subset(good_data,good_data$Date=="2007-02-02")
-   subset_data <- subset_data1
-   subset_data[1441:2880,] <- subset_data2
-   
-   temp <-paste(subset_data[,1], subset_data[,2])
-   subset_data[,1] <- temp
-   
-   temp <- strptime(subset_data[,1] , "%Y-%m-%d %H:%M:%S")
-   subset_data$Date <- temp
-   
-   png(file = "plot4.png", width = 480, height = 480)
-   par(mfrow=c(2,2))
-   plot(x = subset_data$Date , y = as.numeric(subset_data[,3]), type = "l" , xlab = "", ylab = "Global Acive Power")
-   plot(x = subset_data$Date , y = as.numeric(subset_data[,5]), type = "l" , xlab = "datetime", ylab = "Voltage")
-   
-   plot(x = subset_data$Date , y = as.numeric(subset_data[,7]), type = "l" , xlab = "", ylab = "Energy sub metering")
-   points(x = subset_data$Date , y = as.numeric(subset_data[,8]), col = "red", type = "l")
-   points(x = subset_data$Date , y = as.numeric(subset_data[,9]), col = "blue", type = "l")
-   legend(x="topright",legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), 
-          lwd=c(2.5,2.5), bty = "n",col=c("black","red","blue"), y.intersp = 1, xjust = 0.5)
-   
-   plot(x = subset_data$Date , y = as.numeric(subset_data[,4]), type = "l" , xlab = "datetime", ylab = "Global_reactive_power")
-   
-   
-   dev.off()
-   
-   
+# Read column names
+col_names <- names(pow_consumption)
+
+# Limited RAM, hence skip unwanted data rows
+pow_consumption <- read.csv("household_power_consumption.txt",skip=60000,nrows=20000, header=TRUE, sep=";",colClasses = "character")
+
+# Apply column names to read data
+names(pow_consumption) <- col_names
+
+# Convert Date Time columns from character to POSIXct and POSIXt 
+# and embed to data frame
+DateAndTime <- strptime(paste(pow_consumption$Date,pow_consumption$Time), format="%d/%m/%Y %H:%M:%S")
+pow_consumption <- cbind(DateAndTime, pow_consumption)
+
+# Tidy data. Subset data from required dates
+pow_consumption <- rbind(subset(pow_consumption, Date=="1/2/2007"),subset(pow_consumption, Date=="2/2/2007"))
+
+# Convert columns to numeric
+pow_consumption[,4:10] <- sapply(pow_consumption[,4:10],as.numeric)
+
+# Plot 4
+
+# Initiate PNG Device, plot graph and close device
+png("plot4.png", width=480, height=480, units="px")
+
+# Create plot matrix
+par(mfcol=c(2,2))
+
+# 1
+plot(x=pow_consumption$DateAndTime ,y=pow_consumption$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+# 2
+plot(x=pow_consumption$DateAndTime ,y=pow_consumption$Sub_metering_1, type="l", ylab="Energy Sub Metering", xlab="")
+lines(x=pow_consumption$DateAndTime ,y=pow_consumption$Sub_metering_2, col="red")
+lines(x=pow_consumption$DateAndTime ,y=pow_consumption$Sub_metering_3, col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty = 1, col=c("black","red","blue"), bty="n")
+
+# 3
+plot(x=pow_consumption$DateAndTime ,y=pow_consumption$Voltage, type="l", ylab="Voltage", xlab="datetime")
+
+# 4
+plot(x=pow_consumption$DateAndTime ,y=pow_consumption$Global_reactive_power, type="l", ylab="Global Reactive Power", xlab="datetime")
+
+dev.off()
